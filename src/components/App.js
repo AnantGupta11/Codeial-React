d7610b70709184a9b463bdd0bf9ef4466496b032
AnantGupta11 <ak8002749@gmail.com>
2021-05-11 20:24:58 +0530

    Proptypes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
+import React from 'react';
+import {connect} from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import { fetchPosts } from '../actions/posts';
+import { PostsList } from './';
+import PropTypes from 'prop-types';
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+  
+  render() {
+    const { posts } =this.props;
+    console.log('Props',this.props);
+    return (
+      <div>
+        <PostsList posts={posts}/>
+      </div>
+    );
+  }
+}
+function mapStateToProps(state){
+  return {
+    posts:state.posts,
+  };
 }
 
-export default App;
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+}
+export default connect(mapStateToProps)(App);
