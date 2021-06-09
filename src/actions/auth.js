d319f23e0d3703132d80611dc2bdb830f080e503
AnantGupta11 <ak8002749@gmail.com>
2021-06-09 15:19:54 +0530

    Logging user in-3

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,22 +1,45 @@
-import { LOGIN_START } from "./actionTypes";
+import { LOGIN_FAILED, LOGIN_START, LOGIN_SUCCESS } from "./actionTypes";
 import { APIurls } from '../helpers/urls';
-import { getFORMBody } from '../helpers/utils';
+import { getFormBody } from '../helpers/utils';
 
 export function startLogin(){
     return {
         type: LOGIN_START
     }
 };
+export function loginFailed(errorMessage){
+    return {
+        type: LOGIN_FAILED,
+        error: errorMessage
+    }
+}
+export function loginSuccess(user){
+    return {
+        type: LOGIN_SUCCESS,
+        user
+    }
+}
 
 export function login(email,password){
     return (dispatch)=>{
+        dispatch(startLogin());
         const url = APIurls.login();
         fetch(url,{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
-            body: getFORMBody({email, password}),
-        });
+            body: getFormBody({email, password}),
+        })
+        .then(response => response.json())
+        .then((data)=>{
+            console.log('data',data);
+            if(data.success){
+                //dispatch action to save user
+                dispatch(loginSuccess(data.data.user));
+                return;
+            }
+            dispatch(loginFailed(data.message));
+        })
     };
 }
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,13 +1,13 @@
-export function getFormBody(params) {
+export  function getFormBody(params) {
     let formBody = [];
   
     for (let property in params) {
       let encodedKey = encodeURIComponent(property); // 'user name' => 'user%20name'
       let encodedValue = encodeURIComponent(params[property]); // aakash 123 => aakash%2020123
   
-      formBody(encodedKey + '=' + encodedValue);
+      formBody.push(encodedKey + '=' + encodedValue);
     }
   
     return formBody.join('&'); // 'username=aakash&password=123213'
-  }
+}
   
