ce95a333b20b9a827c73b969925165889d3b9da2
AnantGupta11 <ak8002749@gmail.com>
2021-06-10 11:26:14 +0530

    persisting the user-1

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,20 @@ import { BrowserRouter as Router,Link,Route,Switch} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { fetchPosts } from '../actions/posts';
 import { Navbar,Home,Page404,Login,SignUp } from './';
-
+import  jwtDecode from 'jwt-decode';
 
 
 
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
+
+    const token =localStorage.getItem('token');
+
+    if(token){
+        const user=jwtDecode(token);
+        console.log('user',user);
+    }
   }
   
   render() {
